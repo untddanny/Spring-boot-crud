9a021867104622e9a44d4d0c3279421e26d5e1b6
untddanny <126316522+untddanny@users.noreply.github.com>
2023-04-02 13:21:55 +0530

    Add files via upload

---
+++ spring-boot-crud-example-master/src/test/java/com/javatechie/crud/example/SpringBootCrudExample2ApplicationTests.java
@@ -0,0 +1,88 @@
+
+package com.javatechie.crud.example;
+import com.javatechie.crud.example.controller.ProductController;
+import com.javatechie.crud.example.entity.Product;
+import com.javatechie.crud.example.service.ProductService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+
+class SpringBootCrudExample2ApplicationTests {
+//	private com.javatechie.crud.example.entity.Product Product; // edited
+
+
+//	@Test
+//	public void testAddProduct() {
+//		ProductService product = new ProductService();
+//		product.setName("Test");
+//		product.setPrice(123);
+//		product.setQuantity(10);
+//		when(ProductService.saveProduct(product)).thenReturn(product);
+//		ProductService savedProduct = ProductController.addProduct(product);
+//		assertEquals(product, savedProduct);
+//	}
+
+
+//	@Test
+//	void testGetProducts() {
+//		// Given
+//		ProductService product = new ProductService("Apple", 99, 10);
+//
+//		// When
+//		String name = product.getName();
+//		double price = product.getPrice();
+//		int quantity = product.getQuantity();
+//
+//		// Then
+//		assertEquals("Apple", name);
+//		assertEquals(99, price);
+//		assertEquals(10, quantity);
+//	}
+//
+//		@Test
+//		void testAddProducts() {
+//			// Given
+//			ProductService product = new ProductService("Apple", 0.99, 10);
+//			// When
+//
+//			ProductService.saveProduct(product);//
+//			int quantity = product.getQuantity();
+//
+//			// Then
+//			assertEquals(15, quantity);
+//		}
+//
+//		@Test
+//		void testDeleteProducts() {
+//			// Given
+//			ProductService product = new ProductService("Apple", 0.99, 10);
+//
+//			// When
+//			product.deleteProduct(5);
+//			int quantity = product.getQuantity();
+//
+//			// Then
+//			assertEquals(5, quantity);
+//		}
+//
+//		@Test
+//		void testUpdateProduct() {
+//			// Given
+//			ProductService product = new ProductService("Apple", 0.99, 10);
+//
+//			// When
+//			product.updateProduct("Banana", 1.49, 20);
+//			String name = product.getName();
+//			double price = product.getPrice();
+//			int quantity = product.getQuantity();
+//
+//			// Then
+//			assertEquals("Banana", name);
+//			assertEquals(1.49, price);
+//			assertEquals(20, quantity);
+//		}
+}
+
+
